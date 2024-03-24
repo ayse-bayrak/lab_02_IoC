848053e5ca2252f876a8c06c19ab6692affb85ce
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-24 11:51:30 +0300

    little value changes

--- src/main/java/com/cydeo/Currency.java
+++ src/main/java/com/cydeo/Currency.java
@@ -1,11 +1,13 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Currency {
 
     private String code;
--- src/main/java/com/cydeo/config/Config.java
+++ src/main/java/com/cydeo/config/Config.java
@@ -15,15 +15,15 @@ public class Config {
 
     @Bean
     public Currency currency() {
-        return new Currency();
+        return new Currency("001", "Ayse");
     }
 
     @Bean
     public Current current(Currency a) {
-        return new Current(a, BigDecimal.ONE, UUID.randomUUID());
+        return new Current(a, BigDecimal.valueOf(100), UUID.randomUUID());
     }
     @Bean
     public Saving saving(Currency b) {
-        return new Saving(b, BigDecimal.TWO, UUID.randomUUID());
+        return new Saving(b, BigDecimal.valueOf(50), UUID.randomUUID());
     }
 }
